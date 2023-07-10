37d0601030e2b6f95a3b7237227767a3134409a3
Juan <sebastian.garzon@lappiz.io>
2023-07-10 11:41:21 -0500

    Seguidor del mouse

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    console.log('efecto', { enabled })
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    // Limpiar la subscripcion al evento. 
+    // -> Cuando el componente se desmonta
+    // -> Cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      console.log('limpiar')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 40,
+        height: 40, 
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}></div>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse /> 
+    </main>
+  )
+}
+
+export default App
